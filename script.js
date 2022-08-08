0eb2cb27e8ed474583ab51079c098d52a7570f75
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-08-08 21:41:16 +0000

    project files update

---
+++ script.js
@@ -0,0 +1,14 @@
+const display = document.getElementById("numbersSumDisplay");
+const calcNumber = document.getElementById("numBtn");
+const operator =  document.getElementById("operatorBtn");
+const reset = document.getElementById("resetBtn");
+const clearLast = document.getElementById("clearBtn");
+const equalTo = document.getElementById("equalBtn");
+
+
+let displayNum1 = "0";
+let displayNum2 = "0";
+let result = null;
+let decimalDot = false;
+
+
